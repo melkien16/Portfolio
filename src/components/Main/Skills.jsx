c203da749b458fd29e9003ea4eff0aeb614fe02c
melkie <melkieyilk@gmail.com>
2024-12-16 19:29:59 +0300

    skills and contact edit

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -34,7 +34,7 @@ const Skills = () => {
         className="grid lg:grid-cols-3 sm:grid-cols-2 gap-7"
       >
         <div className="skill-container hover:border-l-2 hover:border-r-2 rounded-3xl border-primary transition-all ease-in-out">
-          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block ml-20 mb-8 hover:text-white-500 font-tas transition-all">
+          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block mb-8 hover:text-white-500 font-tas transition-all">
             Languages
           </h1>
           <div className="grid grid-cols-3">
@@ -79,7 +79,7 @@ const Skills = () => {
           </div>
         </div>
         <div className="skill-container hover:border-l-2 hover:border-r-2 rounded-3xl border-primary transition-all ease-in-out">
-          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block ml-20 mb-8 hover:text-white-500 font-tas transition-all">
+          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block lg:ml-20 mb-8 hover:text-white-500 font-tas transition-all">
             Languages and Toos
           </h1>
           <div className="grid grid-cols-3 ">
@@ -91,7 +91,7 @@ const Skills = () => {
                     isHovered && hoveredIndex === id
                       ? "bg-helper-default text-white gap-1"
                       : "text-helper-default bg-transparent gap-10"
-                  } justify-center flex-col p-14 m-2 cursor-pointer transition-all duration-300 ease-in-out`}
+                  } justify-center flex-col p-14 md:m-2 cursor-pointer transition-all duration-300 ease-in-out`}
                   onMouseEnter={() => {
                     setIsHovered(true);
                     setHoveredIndex(id);
@@ -124,8 +124,8 @@ const Skills = () => {
           </div>
         </div>
         <div className="skill-container hover:border-l-2 hover:border-r-2 rounded-3xl border-primary transition-all ease-in-out">
-          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block ml-20 mb-8 hover:text-white-500 font-tas transition-all">
-            Languages and Toos
+          <h1 className="my-3 text-3xl font-bold sub-title relative inline-block lg:ml-20 mb-8 hover:text-white-500 font-tas transition-all">
+            Languages and Tools
           </h1>
           <div className="grid grid-cols-3">
             {Tools.map(({ id, name, className }) => {
