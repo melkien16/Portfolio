25f755c4cb5fab8178fb26e8131568fdb18931c5
melkie <melkieyilk@gmail.com>
2025-01-04 00:15:38 +0300

    working on testimonial section

---
+++ src/components/Main/Testimonials.js
@@ -0,0 +1,34 @@
+const testimonials = [
+  {
+    id: 1,
+    name: "Mr. Lorem Ipsum",
+    feedback: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae justo.",
+    image: "https://via.placeholder.com/100",
+  },
+  {
+    id: 2,
+    name: "Mr. Lorem Ipsum",
+    feedback: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae justo.",
+    image: "https://via.placeholder.com/100",
+  },
+  {
+    id: 3,
+    name: "Mr. Lorem Ipsum",
+    feedback: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae justo.",
+    image: "https://via.placeholder.com/100",
+  },
+  {
+    id: 4,
+    name: "Mr. Lorem Ipsum",
+    feedback: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae justo.",
+    image: "https://via.placeholder.com/100",
+  },
+  {
+    id: 5,
+    name: "Mr. Lorem Ipsum",
+    feedback: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae justo.",
+    image: "https://via.placeholder.com/100",
+  },
+];
+
+export default testimonials;
--- src/components/Main/TestimonialsSection.jsx
+++ src/components/Main/TestimonialsSection.jsx
@@ -1,21 +1,75 @@
 import { motion } from "framer-motion";
-const TestimonialsSection = () => {
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import leftquote from "/quotes/leftquote.png";
+import rightquote from "/quotes/rightquote.png";
+
+import testimonials from "./Testimonials";
+
+const TestimonialSlider = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.95 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 1.5,
-        delay: 0.1,
-        ease: "easeInOut",
-      }}
-      className="h-[500px] p-28 pt-10"
-    >
-      <h1 className="text-3xl font-bold main-title relative inline-block">
-        Testmonial
+    <div className="p-28 pt-10 pb-20 bg-slate-950 dark:bg-slate-200 dark:text-slate-950 text-slate-100">
+      <h1 className="text-3xl font-bold text-center font-poppins m-6 mb-10">
+        Words That Inspire Me
       </h1>
-    </motion.div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={20}
+        slidesPerView={4}
+        navigation
+        pagination={{ clickable: true }}
+        loop
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        {testimonials.map((testimonial) => (
+          <SwiperSlide key={testimonial.id}>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.95 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 1.5,
+                delay: 0.1,
+                ease: "easeInOut",
+              }}
+              className="text-center h-96 p-5 pt-8 shadow-2xl bg-slate-900 dark:bg-transparent rounded-lg mb-20 flex flex-col justify-between"
+            >
+              <div className="text-md">
+                <img src={leftquote} alt="" width={40} />
+
+                <p className="m-4 italic dark:text-[#555] text-gray-100">
+                  {testimonial.feedback}
+                </p>
+                <div className="flex justify-end">
+                  <img src={rightquote} alt="" width={40} />
+                </div>
+              </div>
+              <div className="flex gap-2 justify-start items-center mt-5">
+                <div>
+                  <img
+                    src={testimonial.image}
+                    alt={testimonial.name}
+                    className="rounded-full h-16 w-16 mx-auto mb-5"
+                  />
+                </div>
+                <h3 className="font-tas">{testimonial.name}</h3>
+              </div>
+            </motion.div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
-export default TestimonialsSection;
+export default TestimonialSlider;
