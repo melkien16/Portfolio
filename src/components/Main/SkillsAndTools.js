3561691ae8c686ff5c4095e870338c6b7df2a8c1
melkie <melkieyilk@gmail.com>
2025-01-03 10:37:32 +0300

    add the level of each languages and tools list

--- src/components/Main/SkillsAndTools.js
+++ src/components/Main/SkillsAndTools.js
@@ -3,36 +3,43 @@ export const Languages = [
     id: 1,
     name: "JavaScript",
     className: "devicon-javascript-plain text-6xl",
+    level: "85%",
   },
   {
     id: 2,
     name: "Python",
     className: "devicon-python-plain text-6xl ",
+    level: "65%",
   },
   {
     id: 3,
     name: "Java",
     className: "devicon-java-plain text-6xl",
+    level: "15%",
   },
   {
     id: 4,
     name: "C++",
     className: "devicon-cplusplus-plain text-6xl",
+    level: "75%",
   },
   {
     id: 5,
     name: "C#",
     className: "devicon-csharp-plain text-6xl",
+    level: "25%",
   },
   {
     id: 6,
     name: "HTML5",
     className: "devicon-html5-plain text-6xl",
+    level: "90%",
   },
   {
     id: 7,
     name: "CSS3",
     className: "devicon-css3-plain text-6xl",
+    level: "85%",
   },
 ];
 
@@ -41,21 +48,25 @@ export const Frameworks = [
     id: 8,
     name: "React",
     className: "devicon-react-original text-6xl",
+    level: "65%",
   },
   {
     id: 9,
     name: "Three.js",
     className: "devicon-threejs-original text-6xl",
+    level: "40%",
   },
   {
     id: 10,
     name: "Bootstrap",
     className: "devicon-bootstrap-plain text-6xl",
+    level: "20%",
   },
   {
     id: 11,
     name: "Tailwind",
     className: "devicon-tailwindcss-plain text-6xl",
+    level: "90%",
   },
 ];
 
@@ -64,45 +75,54 @@ export const Tools = [
     id: 12,
     name: "Git",
     className: "devicon-git-plain text-6xl",
+    level: "95%",
   },
   {
     id: 13,
     name: "GitHub",
     className: "devicon-github-original text-6xl",
+    level: "95%",
   },
   {
     id: 14,
     name: "VS Code",
     className: "devicon-vscode-plain text-6xl",
+    level: "99%",
   },
   {
     id: 15,
     name: "Ubuntu",
     className: "devicon-ubuntu-plain text-6xl",
+    level: "70%",
   },
   {
     id: 16,
     name: "MySQL",
     className: "devicon-mysql-plain text-6xl",
+    level: "80%",
   },
   {
     id: 17,
     name: "SQL",
     className: "devicon-microsoftsqlserver-plain text-6xl",
+    level: "85%",
   },
   {
     id: 18,
     name: "Figma",
     className: "devicon-figma-plain text-6xl",
+    level: "70%",
   },
   {
     id: 19,
     name: "Photoshop",
     className: "devicon-photoshop-plain text-6xl",
+    level: "75%",
   },
   {
     id: 20,
     name: "Illustrator",
     className: "devicon-illustrator-plain text-6xl",
+    level: "50%",
   },
 ];
